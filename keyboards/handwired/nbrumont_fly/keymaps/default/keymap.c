6ef1918f5896cedf3b78391a951f5bcf19d8c179
Nicolas Brumont <nicolasbrumont@gmail.com>
2023-05-17 10:48:25 +0200

    updated layout

--- keyboards/handwired/nbrumont_fly/keymaps/default/keymap.c
+++ keyboards/handwired/nbrumont_fly/keymaps/default/keymap.c
@@ -152,9 +152,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                             KC_LGUI, KC_LALT, LCTL_T(KC_SPC), LT(1, KC_BSPC), LT(2, KC_ENT), RCTL_T(KC_TAB)
     ),
 
-
-
-
+/*
+ * L1
+ * TAP
+ * ┌───┬───┬───┬───┬───┐   ┌───┬───┬───┬───┬───┐
+ * │ ° │ ) │ + │ = │ M │   │ œ │   │   │   │DEL│
+ * ├───┼───┼───┼───┼───┤   ├───┤───┼───┼───┼───┤
+ * │ ¨ │ 9 │ 0 │ £ │ µ │   │CAP│ ← │ ↑ │ ↓ │ → │
+ * ├───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┤
+ * │ ^ │ ù │ % │ $ │ m │   │ Œ │VlD│Mut│VlU│ * │
+ * └───┴───┼───┼───┼───┤   ├───┼───┼───┼───┴───┘
+ *         │Win│Alt│Spc│   │   │   │Tab│
+ *         └───┴───┴───┘   └───┴───┴───┘
+*/
 
     [1] = LAYOUT_ortho_3x10_6(
         KC_UNDS,         KC_MINS, KC_PLUS, KC_EQL,  KC_COLN,        KC_GRV,   KC_MRWD, KC_MPLY, KC_MFFD, KC_DEL,
@@ -162,12 +172,41 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LSFT_T(KC_LBRC), KC_QUOT, KC_DQUO, KC_RBRC, KC_SCLN,        KC_TILDE, KC_VOLD, KC_MUTE, KC_VOLU, RSFT_T(KC_BSLS),
                                   KC_LGUI, KC_LALT, LCTL_T(KC_SPC), KC_TRNS,  KC_TRNS, KC_NO
     ),
+
+/*
+ * L2
+ * TAP
+ * ┌───┬───┬───┬───┬───┐   ┌───┬───┬───┬───┬───┐
+ * │ 1 │ 2 │ 3 │ 4 │ 5 │   │ 6 │ 7 │ 8 │ 9 │ 0 │
+ * ├───┼───┼───┼───┼───┤   ├───┤───┼───┼───┼───┤
+ * │ & │ é │ " │ ' │ ( │   │ - │ è │ _ │ ç │ à │
+ * ├───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┤
+ * │   │   │   │   │L3 │   │   │   │ ; │ : │   │
+ * └───┴───┼───┼───┼───┤   ├───┼───┼───┼───┴───┘
+ *         │Win│Alt│Spc│   │   │   │Tab│
+ *         └───┴───┴───┘   └───┴───┴───┘
+*/
+
     [2] = LAYOUT_ortho_3x10_6(
         KC_EXLM, KC_AT, KC_HASH, KC_DLR,  KC_PERC,        KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
         KC_1,    KC_2,  KC_3,    KC_4,    KC_5,           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
         KC_LSFT, KC_NO, KC_NO,   KC_NO,   MO(3),          KC_NO,   KC_NO,   KC_COMM, KC_DOT,  KC_RSFT,
                         KC_LGUI, KC_LALT, LCTL_T(KC_SPC), KC_TRNS, KC_TRNS, KC_NO
     ),
+
+    /*
+ * L3
+ * TAP
+ * ┌───┬───┬───┬───┬───┐   ┌───┬───┬───┬───┬───┐
+ * │   │   │   │   │   │   │   │   │   │   │   │
+ * ├───┼───┼───┼───┼───┤   ├───┤───┼───┼───┼───┤
+ * │F1 │F2 │F3 │F4 │F5 │   │F6 │F7 │F8 │F9 │F10│
+ * ├───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┤
+ * │F11│   │   │   │   │   │   │   │   │   │F12│
+ * └───┴───┼───┼───┼───┤   ├───┼───┼───┼───┴───┘
+ *         │Win│Alt│Spc│   │   │   │BLD│
+ *         └───┴───┴───┘   └───┴───┴───┘
+*/
     [3] = LAYOUT_ortho_3x10_6(
         KC_NO,  KC_NO, KC_NO,   KC_NO,   KC_NO,          KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, 
         KC_F1,  KC_F2, KC_F3,   KC_F4,   KC_F5,          KC_F6,   KC_F7,   KC_F8, KC_F9, KC_F10,
