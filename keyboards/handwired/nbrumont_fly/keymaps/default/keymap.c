703db1daeecc993b849e581704c101f66cb70c42
Nicolas Brumont <nicolasbrumont@gmail.com>
2023-05-19 15:46:30 +0200

    format()

--- keyboards/handwired/nbrumont_fly/keymaps/default/keymap.c
+++ keyboards/handwired/nbrumont_fly/keymaps/default/keymap.c
@@ -146,9 +146,9 @@ void x_reset(tap_dance_state_t *state, void *user_data);
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ortho_3x10_6(
-        KC_Q,         KC_W, KC_E,    KC_R,    KC_T,           KC_Y,                 KC_U,           KC_I,                           KC_O,   KC_P,
-        KC_A,         KC_S, KC_D,    KC_F,    KC_G,           KC_H,                 KC_J,           KC_K,                           KC_L,   KC_SCLN,
-        LSFT_T(KC_Z), KC_X, KC_C,    KC_V,    KC_B,           KC_N,                 KC_M,           KC_COMMA,                       KC_DOT, RSFT_T(KC_SLSH),
+        KC_Q,         KC_W, KC_E,    KC_R,    KC_T,           KC_Y,                 KC_U,           KC_I,           KC_O,   KC_P,
+        KC_A,         KC_S, KC_D,    KC_F,    KC_G,           KC_H,                 KC_J,           KC_K,           KC_L,   KC_SCLN,
+        LSFT_T(KC_Z), KC_X, KC_C,    KC_V,    KC_B,           KC_N,                 KC_M,           KC_COMMA,       KC_DOT, RSFT_T(KC_SLSH),
                             KC_LGUI, KC_LALT, LCTL_T(KC_SPC), LT(1, KC_SPC),        RALT_T(KC_ENT), RCTL_T(KC_TAB)
     ),
 
@@ -180,7 +180,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [1] = LAYOUT_ortho_3x10_6(
         KC_1,    KC_2,  KC_3,     KC_4,    KC_5,              KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-        KC_ESC,  KC_NO, KC_NO,    KC_EQL,  KC_MINS,           KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT,  KC_BSPC,
+        KC_ESC,  KC_NO, KC_NO,    KC_EQL,  KC_MINS,           KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, KC_BSPC,
         KC_EXLM, KC_AT, KC_HASH,  KC_DLR,  KC_PERC,           KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
                         KC_LSFT,  KC_RALT, LT(2, KC_SPC),     KC_TRNS, KC_RALT, KC_NO
     ),
