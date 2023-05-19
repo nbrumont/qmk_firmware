48433bae5ccc539c101c8dbbe69f5bed8504f52a
Nicolas Brumont <nicolasbrumont@gmail.com>
2023-05-19 15:24:17 +0200

    feat(): mapped escape

--- keyboards/handwired/nbrumont_fly/keymaps/default/keymap.c
+++ keyboards/handwired/nbrumont_fly/keymaps/default/keymap.c
@@ -149,7 +149,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,         KC_W, KC_E,    KC_R,    KC_T,           KC_Y,                 KC_U,           KC_I,                           KC_O,   KC_P,
         KC_A,         KC_S, KC_D,    KC_F,    KC_G,           KC_H,                 KC_J,           KC_K,                           KC_L,   KC_SCLN,
         LSFT_T(KC_Z), KC_X, KC_C,    KC_V,    KC_B,           KC_N,                 KC_M,           KC_COMMA,                       KC_DOT, RSFT_T(KC_SLSH),
-                            KC_LGUI, KC_LALT, LCTL_T(KC_SPC), LT(1, KC_SPC),       RALT_T(KC_ENT), RCTL_T(KC_TAB)
+                            KC_LGUI, KC_LALT, LCTL_T(KC_SPC), LT(1, KC_SPC),        RALT_T(KC_ENT), RCTL_T(KC_TAB)
     ),
 
 
@@ -159,7 +159,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ┌───┬───┬───┬───┬───┐   ┌───┬───┬───┬───┬───┐
  * │ & │ é │ " │ ' │ ( │   │ - │ è │ _ │ ç │ à │
  * ├───┼───┼───┼───┼───┤   ├───┤───┼───┼───┼───┤
- * │ ; │ : │   │ = │ ) │   │ ← │ ↑ │ ↓ │ → │Bsp│
+ * │ESC│   │   │ = │ ) │   │ ← │ ↑ │ ↓ │ → │Bsp│
  * ├───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┤
  * │ 1 │ 2 │ 3 │ 4 │ 5 │   │ 6 │ 7 │ 8 │ 9 │ 0 │
  * └───┴───┼───┼───┼───┤   ├───┼───┼───┼───┴───┘
@@ -170,7 +170,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ┌───┬───┬───┬───┬───┐   ┌───┬───┬───┬───┬───┐
  * │   │ ~ │ # │ { │ [ │   │ | │ ` │ \ │ ^ │ @ │
  * ├───┼───┼───┼───┼───┤   ├───┤───┼───┼───┼───┤
- * │ × │ ÷ │   │ } │ ] │   │ ← │ ↑ │ ↓ │ → │   │
+ * │ESC│   │   │ } │ ] │   │ ← │ ↑ │ ↓ │ → │   │
  * ├───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┤
  * │ · │ É │ ¸ │ ´ │ ¨ │   │ ¦ │ È │ ¯ │ Ç │ À │
  * └───┴───┼───┼───┼───┤   ├───┼───┼───┼───┴───┘
@@ -179,16 +179,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 */
 
     [1] = LAYOUT_ortho_3x10_6(
-        KC_1,    KC_2,  KC_3,    KC_4,    KC_5,              KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-        KC_COMM, KC_DOT, KC_NO,  KC_EQL,  KC_MINS,           KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT,  KC_BSPC,
-        KC_EXLM, KC_AT, KC_HASH, KC_DLR,  KC_PERC,           KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-                        KC_LSFT, KC_RALT, LT(2, KC_SPC),     KC_TRNS, KC_RALT, KC_NO
+        KC_1,    KC_2,  KC_3,     KC_4,    KC_5,              KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+        KC_ESC,  KC_NO, KC_NO,    KC_EQL,  KC_MINS,           KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT,  KC_BSPC,
+        KC_EXLM, KC_AT, KC_HASH,  KC_DLR,  KC_PERC,           KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
+                        KC_LSFT,  KC_RALT, LT(2, KC_SPC),     KC_TRNS, KC_RALT, KC_NO
     ),
 /*
  * L2
  * TAP
  * ┌───┬───┬───┬───┬───┐   ┌───┬───┬───┬───┬───┐
- * │ ° │ ) │ + │ = │ M │   │ œ │   │   │   │CAP│
+ * │ ° │ ) │ + │ = │ M │   │ œ │Pau│   │   │CAP│
  * ├───┼───┼───┼───┼───┤   ├───┤───┼───┼───┼───┤
  * │ ¨ │ 9 │ 0 │ £ │ µ │   │ ← │ ↑ │ ↓ │ → │DEL│
  * ├───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┤
@@ -210,7 +210,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 */
 
     [2] = LAYOUT_ortho_3x10_6(
-        KC_UNDS,         KC_MINS, KC_PLUS, KC_EQL,  KC_COLN,        KC_GRV,          KC_MPLY, KC_NO,   KC_ESC,  KC_CAPS,
+        KC_UNDS,         KC_MINS, KC_PLUS, KC_EQL,  KC_COLN,        KC_GRV,          KC_MPLY, KC_NO,   KC_NO,   KC_CAPS,
         KC_LCBR,         KC_LPRN, KC_RPRN, KC_RCBR, KC_PIPE,        KC_LEFT,         KC_UP,   KC_DOWN, KC_RGHT, KC_DEL,
         LSFT_T(KC_LBRC), KC_QUOT, KC_DQUO, KC_RBRC, KC_SCLN,        KC_TILDE,        KC_VOLD, KC_MUTE, KC_VOLU, RSFT_T(KC_BSLS),
                                   KC_LGUI, KC_LALT, KC_TRNS,        LT(3, KC_BSPC),  KC_RALT, KC_NO
